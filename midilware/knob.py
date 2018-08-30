a2e19dde0b1d615ff840f76286da42c19b9cf715
Benjamin Browning <brbrowning21@gmail.com>
2018-08-29 22:48:50 -0400

    Keep track of knob state

--- midilware/knob.py
+++ midilware/knob.py
@@ -10,16 +10,16 @@ class Knob:
         self.cc = cc
         self.channel = channel
 
-    def run(self):
-        for msg in self.in_p:
-            if msg.value == 63:
-                if self.v <= 0:
-                    self.v = 0
-                else:
-                    self.v -= 1
-            elif msg.value == 65:
-                if self.v > 126:
-                    self.v = 126
-                else:
-                    self.v += 1
-            self.out_p.send(mido.Message('control_change', control=self.cc, value=self.v, channel=self.channel))
+    def handle(self, msg):
+        if msg.value == 63:
+            print("dec")
+            if self.v <= 0:
+                self.v = 0
+            else:
+                self.v -= 1
+        elif msg.value == 65:
+            print("inc")
+            if self.v > 126:
+                self.v = 126
+            else:
+                self.v += 1
